f6d7391f6e86e4e5476b4e398728ece18dbd4941
TaeyeongHan <dianspace53@gmail.com>
2025-04-18 15:14:13 +0900

    push

---
+++ Assets/05_Character/Shader/test.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        Console.WriteLine("d");
+        Console.WriteLine("d");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
