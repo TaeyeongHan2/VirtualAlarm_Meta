3a32ef691ee77b49a72ff755a390ded95de74ac9
carlton368 <carlton368@gmail.com>
2025-04-19 13:20:21 +0900

    ADD : 알람 드래그시 삭제 기능

--- Assets/03_Scripts/Alarm/AlarmButton.cs
+++ Assets/03_Scripts/Alarm/AlarmButton.cs
@@ -2,18 +2,50 @@ using System;
 using _03_Scripts.Alarm;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class AlarmButton : MonoBehaviour
+public class AlarmButton : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public TMP_Text textUI;
-
+    public Image buttonImage;
+    private String _textBeforeDraged;
+    public float dragCheckSensitivity = 100f;
+    public String UITextOnDraging = "Swape to Delete"; 
     private void Awake()
     {
         textUI = GetComponentInChildren<TMP_Text>();
+        buttonImage = GetComponentInChildren<Image>();
     }
 
     public void SetData(AlarmBase alarmBase)
     {
         textUI.text = $"{alarmBase.alarm12time}";
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        _textBeforeDraged = textUI.text;
+        textUI.text = UITextOnDraging;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        //var originalPosition = eventData.position;
+
+        if (eventData.delta.sqrMagnitude > dragCheckSensitivity)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        if (this != null)
+            this.textUI.text = _textBeforeDraged;
     }
 }
