02d15fafef84aed7dab5b1592d951e50d0847fed
TaeyeongHan <dianspace53@gmail.com>
2025-04-20 00:22:00 +0900

    알람이 울린 후 캐릭터가 하품하며 일어나서 팔을 흔들며 인사하는 모션 제작 완료

---
+++ Assets/03_Scripts/Character/AfterAlarmShinanoFaceController.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using System.Collections;
+
+public class AfterAlarmShinanoFaceController : MonoBehaviour
+{
+   private Animator animator;
+
+   private void Start()
+   {
+       StartCoroutine(FaceSequence());
+   }
+
+   IEnumerator FaceSequence()
+   {
+       animator.SetLayerWeight(animator.GetLayerIndex("Facial Layer"), 0);
+       
+       yield return new WaitForSeconds(1.5f);
+       animator.SetLayerWeight(animator.GetLayerIndex("Facial Layer"), 1);
+       animator.SetInteger("FaceState", 1);
+       
+       yield return new WaitForSeconds(0.3f);
+       animator.SetInteger("FaceState", 2);
+   }
+}
